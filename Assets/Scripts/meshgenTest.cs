fbe380401d01e3e0cb0c465ae8204b7c88ae08f9
Basil <nicolas1gerard@hotmail.fr>
2020-02-21 13:04:52 +0100

    experiments

---
+++ Assets/Scripts/meshgenTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class meshgenTest : MonoBehaviour
+{
+    Vector3[] vertices = new Vector3[4];
+    Vector2[] uvs = new Vector2[4];
+    int[] triangles = new int[6];
+    Mesh mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vertices[0] = new Vector3(0f, 0f, 0f);
+        vertices[1] = new Vector3(1.0f, 0f, 0f);
+        vertices[2] = new Vector3(1.0f, 1.0f, 0f);
+        vertices[3] = new Vector3(0f, 1.0f, 0f);
+        uvs[0] = new Vector2(0f,1f);
+        uvs[1] = new Vector2(1.0f,1f);
+        uvs[2] = new Vector2(1.0f,0f);
+        uvs[3] = new Vector2(0f,0f);
+        triangles[0]=0;
+        triangles[1]=3;
+        triangles[2]=2;
+        triangles[3]=0;
+        triangles[4]=2;
+        triangles[5]=1;
+        mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.uv = uvs;
+        mesh.triangles = triangles;
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/polygonTracer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class polygonTracer : MonoBehaviour
+{
+    public enum TraceDirection
+    {
+        side,
+        front,
+        wings
+    }
+    [HideInInspector]public TraceDirection direction;
+    [Header("Settings")]
+    public float tracingStep = 1.0f;//spatial period of tracing sampling
+    public float closingDistance = 1.0f;//maximum distance the game will try to close the loop of the players tracing
+    bool isTracing = false;
+    Mesh mesh;
+    Vector3[] sideTracePoints;
+    Vector3[] frontTracePoints;
+    Vector3[] WingTracePoints;
+    private LineRenderer line;
+    public IEnumerator Trace()
+    {
+        isTracing = true;
+        Vector3 previousposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        List<Vector3> points = new List<Vector3>();
+        points.Add(previousposition);
+        while (Input.GetMouseButton(0))//keep looping as long as the user keeps the button pressed
+        {
+            if(Vector3.Distance(previousposition, Camera.main.ScreenToWorldPoint(Input.mousePosition)) > tracingStep)//add a point in the tracing
+            {
+                previousposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                points.Add(previousposition);
+                line.SetPositions(points.ToArray());
+            }
+            yield return null;
+        }
+
+        switch (direction)
+        {
+            case (TraceDirection.side):
+                sideTracePoints = points.ToArray();
+                break;
+            case (TraceDirection.front):
+                frontTracePoints = points.ToArray();
+                break;
+            case (TraceDirection.wings):
+                WingTracePoints = points.ToArray();
+                break;
+            default:
+                Debug.LogError("Invalid tracing direction/type");
+                break;
+
+        }
+        isTracing = false;// swap this for an event?
+    }
+
+    public Mesh GetMesh()//this should give a copy, not a reference
+    {
+        return Instantiate(mesh);
+    }
+}
